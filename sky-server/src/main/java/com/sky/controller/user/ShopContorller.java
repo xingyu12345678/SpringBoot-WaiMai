f754e9f56b79f48b579179e946d2a1d4e9192140
Xinxin <lxx123dyx@163.com>
2023-12-26 01:15:01 +0800

    苍鸿外卖day6微信小程序浏览部分完成

--- sky-server/src/main/java/com/sky/controller/user/ShopContorller.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopContorller.java
@@ -9,7 +9,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/user/shop")
-@RestController("userContorller")
+@RestController("UserContorller")
 @Slf4j
 @Api(tags = "店铺相关接口")
 public class ShopContorller {
